f819ee1cfd66fea8e8fde2398038459bb5310381
morethanmin <mini4614@gmail.com>
2021-06-20 12:46:52 +0900

    react masronry

--- src/components/MemoLIst/Memo.js
+++ src/components/MemoLIst/Memo.js
@@ -4,10 +4,8 @@ const { media } = require("lib/style-utils");
 const { default: styled } = require("styled-components");
 
 const Contents = styled.div`
-  display: inline-block;
   width: 100%;
   padding: 1rem 1.5rem;
-  box-sizing: border-box;
   border: 1px solid #e0e0e0;
   border-radius: 10px;
   min-height: 104px;
--- src/components/MemoLIst/MemoList.js
+++ src/components/MemoLIst/MemoList.js
@@ -1,37 +1,44 @@
 import { media, transitions } from "lib/style-utils";
 import Memo from "./Memo";
+import Masonry from "react-masonry-css";
 
 const { default: styled } = require("styled-components");
 
-const Wrapper = styled.div`
-  margin-top: 3rem;
-  display: grid;
-  grid-template-columns: repeat(${({ columns }) => columns}, 1fr);
-  grid-gap: 1rem;
-`;
+const breakpointColumnsObj = {
+  default: 5,
+  1300: 4,
+  1100: 3,
+  700: 2,
+  500: 1,
+};
 
-const Column = styled.div`
-  display: grid;
-  grid-gap: 0.6rem;
-  grid-auto-rows: max-content;
+const StyledMasonry = styled(Masonry)`
+  margin-top: 3rem;
+  display: flex;
+  margin-left: -30px; /* gutter size offset */
+  width: auto;
+  .my-masonry-grid_column {
+    padding-left: 1rem;
+  }
 `;
 
 const MemoList = ({ memos, onOpen }) => {
   const memoList = memos.map((memo) => (
     <Memo key={memo.id} memo={memo} onOpen={onOpen} />
   ));
-
-  const output = memoList.reduce((acc, child, i) => {
-    acc[i % 3] = [...acc[i % 3], child];
-    return acc;
-  }, new Array(3).fill([]));
-
   return (
-    <Wrapper columns={3}>
-      {output.map((column, i) => (
-        <Column key={i}>{column}</Column>
-      ))}
-    </Wrapper>
+    <StyledMasonry
+      breakpointCols={breakpointColumnsObj}
+      className="my-masonry-grid"
+      columnClassName="my-masonry-grid_column"
+    >
+      {memoList}
+    </StyledMasonry>
+    // <Wrapper columns={3}>
+    //   {output.map((column, i) => (
+    //     <Column key={i}>{column}</Column>
+    //   ))}
+    // </Wrapper>
   );
 };
 
